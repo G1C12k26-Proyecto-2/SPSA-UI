bebf7e65c0f582d9232de1373220f6f1c64fc18f
Daniel Fonseca <josedaniel0102@hotmail.com>
2026-03-27 19:10:27 -0600

    Redireccion al login al iniciar programa

--- WEB_UI/Controllers/HomeController.cs
+++ WEB_UI/Controllers/HomeController.cs
@@ -1,24 +1,12 @@
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
-using WEB_UI.Models;
 
 namespace WEB_UI.Controllers
 {
     public class HomeController : Controller
     {
-     //   [Authorize]
         public IActionResult Index()
         {
-            var rol = User.Claims.FirstOrDefault(c => c.Type == "Rol")?.Value;
-
-            return rol?.ToLower() switch
-            {
-                "Admin" => RedirectToAction("Dashboard", "Admin"),
-                "Funcionario" => RedirectToAction("Index", "IngForestal"),
-                "Propietario" => RedirectToAction("Index", "Fincas"),
-                _ => View()
-            };
+            return RedirectToAction("Login", "Login");
         }
     }
 }
