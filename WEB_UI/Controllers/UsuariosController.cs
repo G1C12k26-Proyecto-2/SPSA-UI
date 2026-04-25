d5ffde112cc939fe20762f8e9b8c99f956f29f39
Trey0594 <tgonzalezb@ucenfotec.ac.cr>
2026-04-25 01:21:06 -0600

    GestionUsuarios

--- WEB_UI/Controllers/UsuariosController.cs
+++ WEB_UI/Controllers/UsuariosController.cs
@@ -10,7 +10,7 @@ namespace WebApplication1.Controllers
         }
         public IActionResult Index() => View();
         public IActionResult Crear() => View();
-        public IActionResult Editar() => View();
+        public IActionResult Editar(int id) => View();
 
 
     }
